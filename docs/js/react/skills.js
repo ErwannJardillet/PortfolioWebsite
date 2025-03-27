a7e89a132b54289c140905117317e1351b3ce0b0
jardille <@jardille>
2025-03-27 18:49:19 +0100

    Add carousel feature to Skills section: implement logo scrolling effect and update styles

--- docs/js/react/skills.js
+++ docs/js/react/skills.js
@@ -1,7 +1,20 @@
 // React component for the "Skills" section
 function Skills() {
+    const logos = [
+        "c.png", "css.png", "html.png", "javascipt.png",
+        "java.png", "python.png", "rust.png", "php.png",
+        "react.png", "git.png", "figma.png"
+    ];
+
+    const repeatedLogos = [...logos, ...logos];
+
     return (
         <section id="skills">
+            <div className="carousel-track">
+                {repeatedLogos.map((logo, index) => (
+                    <img key={index} src={`/docs/img/${logo}`} alt={logo.replace(".png", "")} />
+                ))}
+            </div>
         </section>
     );
 }
