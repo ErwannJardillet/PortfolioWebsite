3b245a073ae2fb48e5c3e743b69782921bf4d377
jardille <@jardille>
2025-03-27 15:19:58 +0100

    about me done , and img add for skills

--- docs/js/react/aboutMe.js
+++ docs/js/react/aboutMe.js
@@ -2,6 +2,15 @@
 function AboutMe() {
     return (
         <section id="aboutMe">
+            <div class="pic">
+                <img src="/docs/img/me.png" alt="Photo de moi" />
+                <p>Jardillet<br/> Erwann</p>
+            </div>
+            <div class="content">
+                <div class="text1"> <p>Passionné par le développement web et les nouvelles technologies, je suis constamment à la recherche de nouveaux défis pour améliorer mes compétences. Curieux, rigoureux et créatif, j’aime transformer des idées en projets concrets et fonctionnels.</p></div>
+                <div class="line"></div>
+                <div class="text2"> <p>Mon objectif est de contribuer à des projets innovants tout en continuant d’apprendre chaque jour. J’accorde une grande importance au travail d’équipe, à l’écoute active et à la qualité du code. Chaque ligne que j’écris vise à avoir un impact positif et durable.</p></div>
+            </div>
         </section>
     );
 }
