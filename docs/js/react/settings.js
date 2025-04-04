0510071c5a16fcec91ba4f5125049252f6446788
jardille <@jardille>
2025-04-04 14:04:45 +0200

    Refactor Settings component for improved theme selection and layout adjustments

--- docs/js/react/settings.js
+++ docs/js/react/settings.js
@@ -1,45 +1,48 @@
 function Settings() {
-    const [pageIndex, setPageIndex] = React.useState(0);
-  
-    const pages = [
-      <div className="start" key="start">
-        <h1>Paramètres</h1>
-        <img src="./img/settings.png"></img>
-      </div>,
-      <div className="theme" key="theme">
-        <h1>Thème</h1>
+  const [pageIndex, setPageIndex] = React.useState(0);
 
-        <div class = "theme_selector">
-            <div class="theme1">
-                
-            </div>
-        </div>
+  const pages = [
+    <div className="start" key="start">
+      <h1>Paramètres</h1>
+      <img src="./img/settings.png"></img>
+    </div>,
+    <div className="theme" key="theme">
+      <h1>Thème</h1>
 
-      </div>,
-      <div className="language" key="langue">
-        <h1>Langue</h1>
+      <div class="theme_selector">
+        <div class="theme1"></div>
+        <div class="theme2"></div>
+        <div class="theme3"></div>
+        <div class="theme4"></div>
+        <div class="theme5"></div>
+        <div class="theme6"></div>
       </div>
-    ];
-  
-    const nextPage = () => {
-      setPageIndex((prevIndex) => (prevIndex + 1) % pages.length);
-    };
-  
-    const prevPage = () => {
-      setPageIndex((prevIndex) => (prevIndex - 1 + pages.length) % pages.length);
-    };
-  
-    return (
-      <section className="settings_content">
-        <div className="left" onClick={prevPage}><p>❮</p></div>
-        <div className="center">
-          {pages[pageIndex]}
-        </div>
-        <div className="right" onClick={nextPage}><p>❯</p></div>
-      </section>
-    );
-  }
-  
-  const root = ReactDOM.createRoot(document.getElementById("settings"));
-  root.render(<Settings />);
-  
+    </div>,
+    <div className="language" key="langue">
+      <h1>Langue</h1>
+    </div>,
+  ];
+
+  const nextPage = () => {
+    setPageIndex((prevIndex) => (prevIndex + 1) % pages.length);
+  };
+
+  const prevPage = () => {
+    setPageIndex((prevIndex) => (prevIndex - 1 + pages.length) % pages.length);
+  };
+
+  return (
+    <section className="settings_content">
+      <div className="left" onClick={prevPage}>
+        <p>❮</p>
+      </div>
+      <div className="center">{pages[pageIndex]}</div>
+      <div className="right" onClick={nextPage}>
+        <p>❯</p>
+      </div>
+    </section>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("settings"));
+root.render(<Settings />);
